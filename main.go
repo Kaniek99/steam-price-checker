0e3dbd1df457ae0b193a6bd7a88db78bd9b5d7b0
Kaniek <kaniek99@gmail.com>
2023-07-26 13:30:23 +0200

    feat: add reading and setting environment values from .env

--- main.go
+++ main.go
@@ -1,5 +1,40 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
+func main() {
+	SetEnvironmentVariables()
+}
+
+func SetEnvironmentVariables() {
+	file, err := os.Open(".env")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+
+	environmentValues := make(map[string]string)
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		envValue := strings.Split(line, "=")
+		key := strings.TrimSpace(envValue[0])
+		value := strings.TrimSpace(envValue[1])
+		environmentValues[key] = value
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println(err)
+	}
+
+	for key, value := range environmentValues {
+		os.Setenv(key, value)
+	}
 }
